57855a89c96af80e2cfa524d38bcfda5ad1282cf
kaemzy <52842288+kaemzy@users.noreply.github.com>
2020-05-31 14:34:16 -0700

    afti
    afti

---
+++ Welcome.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Welcome : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["Username"] != null)
+        {
+            Response.Write("Welcome  " + Session["Username"]);
+        }
+    }
+    protected void btnlogout_Click(object sender, EventArgs e)
+    {
+        Session.Remove("Username");
+        Response.Redirect("Default.aspx");
+    }
+}
